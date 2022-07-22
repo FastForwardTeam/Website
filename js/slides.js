ab13b05e3028ef3680f89887f21e4d22f2753c2c
Princess Mortix <47502554+princessmortix@users.noreply.github.com>
2022-07-21 21:15:39 -0300

    Add missing meta tags and logo (#26)
    * Add missing meta tags
    
    * Add missing meta tags
    
    * Add missing meta tags
    
    * Add missing meta tags
    
    * Add missing meta tags
    
    * Add button to see FF in action & meta tags
    
    * Add missing meta tags
    
    * Add missing meta tags
    
    * Add missing meta tags
    
    * Add missing meta tags
    
    * Update slides.js
    
    * Revert slides.js

--- js/slides.js
+++ js/slides.js
@@ -16,7 +16,7 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  slides[slideIndex-1].style.display = "block";  
+    slides[slideIndex-1].style.display = "block";  
 }
 
 interval = setInterval(plusSlides, 4000);
