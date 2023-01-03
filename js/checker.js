58e51071ef3e022d722f13fb7fca953f77df5cc5
AliahX <40045439+AliahX@users.noreply.github.com>
2023-01-03 14:25:25 +1300

    Replace avaliable tag with mv2 tag

--- js/checker.js
+++ js/checker.js
@@ -10,12 +10,11 @@ fetch("https://raw.githubusercontent.com/FastForwardTeam/FastForward/manifest-v3
   })
 });
 
-function values(b, c, d, e, f) {
-  document.getElementById("bypass").innerHTML = b;
-  document.getElementById("available").innerHTML = c;
-  document.getElementById("mv2").innerHTML = d;
-  document.getElementById("mv3").innerHTML = e;
-  document.getElementById("extraInfo").innerHTML = f;
+function values(a, b, c, d) {
+  document.getElementById("bypass").innerHTML = a;
+  document.getElementById("mv2").innerHTML = b;
+  document.getElementById("mv3").innerHTML = c;
+  document.getElementById("extraInfo").innerHTML = d;
 }
 
 function check() {
@@ -26,18 +25,18 @@ function check() {
       if(bypass[0] == "(") {
         regex = new RegExp(".*" + bypass.slice(2, -2).replace(/\//g, "\\/").replace(/https:\/\//g, "") + ".*")
         if(regex.test(url)) {
-          values(bypass, (bypasses[bypass] == undefined || bypasses[bypass] == "C") ? "✓" : "✗", bypasses[bypass] == undefined || bypasses[bypass] == "C" ? "✓" : "✗", bypasses[bypass] == "C" ? "✓" : "✗", bypasses[bypass] == "X" ? "We no longer bypass this site because it is no longer active, or harmful to your computer. Dont think so? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>" : "Bypass not working? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>");
+          values(bypass, (bypasses[bypass] == undefined || bypasses[bypass] == "C") ? "✓" : "✗", bypasses[bypass] == "C" ? "✓" : "✗", bypasses[bypass] == "X" ? "We no longer bypass this site because it is no longer active, or harmful to your computer. Dont think so? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>" : "Bypass not working? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>");
           return;
         }
       }
       if(bypass.includes(url) || url.includes(bypass)) {
-        values(bypass, (bypasses[bypass] == undefined || bypasses[bypass] == "C") ? "✓" : "✗", bypasses[bypass] == undefined ? "✓" : "✗", bypasses[bypass] == "C" ? "✓" : "✗", bypasses[bypass] == "X" ? "We no longer bypass this site because it is no longer active, or harmful to your computer. Dont think so? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>" : "Bypass not working? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>");
+        values(bypass, (bypasses[bypass] == undefined || bypasses[bypass] == "C") ? "✓" : "✗", bypasses[bypass] == "C" ? "✓" : "✗", bypasses[bypass] == "X" ? "We no longer bypass this site because it is no longer active, or harmful to your computer. Dont think so? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>" : "Bypass not working? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>");
         return;
       }
     }
-    values("No bypasses found", "✗", "✗", "✗", "There is no bypass for this site. <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub?</a>");
+    values("No bypasses found", "✗", "✗", "There is no bypass for this site. <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub?</a>");
   } else {
-    values("", "", "", "", "");
+    values("", "", "", "");
   }
 }
 
