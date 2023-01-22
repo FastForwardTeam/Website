4032fbdd879760c9aed3268880a1f80a235e2465
AliahX <40045439+AliahX@users.noreply.github.com>
2023-01-23 11:14:58 +1300

    update to use Bypassed.md instead of bypasssites.md

--- js/checker.js
+++ js/checker.js
@@ -1,11 +1,12 @@
 bypasses = {}
-fetch("https://raw.githubusercontent.com/FastForwardTeam/FastForward/manifest-v3/bypasssites.md").then((response) => {
+fetch("https://raw.githubusercontent.com/FastForwardTeam/FastForward/manifest-v3/Bypassed.md").then((response) => {
   response.text().then((data) => {
-    data = data.replace(/  \n/g, "\n").replace(/\\/g, "").split("\n");
-    data.splice(0, 6);
+    data = data.split("\n");
+    data.splice(0, 16);
     data.pop();
     for (i = 0; i < data.length; i++) {
-      bypasses[data[i].split(" ")[0]] = data[i].split(" ")[1];
+      data[i].replace(/ /g, "").split("|").splice(1, 2)
+      bypasses[data[i].replace(/ /g, "").split("|")[1]] = data[i].replace(/ /g, "").split("|")[2];
     }
   })
 });
@@ -22,15 +23,8 @@ function check() {
   if(url != "") {
     for(i = 0; i < Object.keys(bypasses).length; i++) {
       bypass = Object.keys(bypasses)[i];
-      if(bypass[0] == "(") {
-        regex = new RegExp(".*" + bypass.slice(2, -2).replace(/\//g, "\\/").replace(/https:\/\//g, "") + ".*")
-        if(regex.test(url)) {
-          values(bypass, (bypasses[bypass] == undefined || bypasses[bypass] == "C") ? "✓" : "✗", bypasses[bypass] == "C" ? "✓" : "✗", bypasses[bypass] == "X" ? "We no longer bypass this site because it is no longer active, or harmful to your computer. Dont think so? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>" : "Bypass not working? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>");
-          return;
-        }
-      }
       if(bypass.includes(url) || url.includes(bypass)) {
-        values(bypass, (bypasses[bypass] == undefined || bypasses[bypass] == "C") ? "✓" : "✗", bypasses[bypass] == "C" ? "✓" : "✗", bypasses[bypass] == "X" ? "We no longer bypass this site because it is no longer active, or harmful to your computer. Dont think so? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>" : "Bypass not working? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>");
+        values(bypass, (bypasses[bypass] == undefined || bypasses[bypass][0] == "✅") ? "✓" : "✗", bypasses[bypass][0] == "✅" ? "✓" : "✗", bypasses[bypass][0] == "🛑" ? "We no longer bypass this site because it is no longer active, or harmful to your computer. Dont think so? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>" : "Bypass not working? <a class='link' href='https://github.com/FastForwardTeam/FastForward/issues/new'>Open an issue on GitHub</a>");
         return;
       }
     }
