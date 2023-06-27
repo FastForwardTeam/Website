09033d20c15103a384afec10ec362a19c15ed81f
AliahX <40045439+AliahX@users.noreply.github.com>
2023-06-27 18:39:45 +1200

    update the thing bc i missed a line ;w;

--- js/instructions.js
+++ js/instructions.js
@@ -30,9 +30,11 @@ function loadEndPageContent() {
     2. Unzip (extract) the zip file.<br>
     This should leave you with a single file named <code>FastForward_chromium_0.XXXX.zip</code>.<br>
     If the file is called <code>FastForward_chromium.zip</code>, you have not properly extracted it and will need to try again.<br>
-    3. Open <code>${browser == "Opera" ? "opera" : browser == "Brave" ? "brave" : "chrome"}://extensions</code><br>
-    4. Enable developer mode, which can be found in the top right corner.<br>
-    5. Drag the <code>FastForward_chromium_0.XXXX</code> folder that you extracted into the extentions page.<br>
+    3. Extract the zip file again.<br>
+    This should leave you with a folder named <code>FastForward_chromium_0.XXXX</code>.<br>
+    4. Open <code>${browser == "Opera" ? "opera" : browser == "Brave" ? "brave" : "chrome"}://extensions</code><br>
+    5. Enable developer mode, which can be found in the top right corner.<br>
+    6. Drag the <code>FastForward_chromium_0.XXXX</code> folder that you extracted into the extentions page.<br>
     FastForward should now be installed!`,
 
 //     "Firefox" : `<p>Unfortunately, FastForward was removed from the Firefox Store. You will have to manually install it instead.</p>
