b9e359f9cd090a7aa88623ecbe120c4bc9ca16e8
Jose Garcia <47431411+ruxwez@users.noreply.github.com>
2024-05-16 18:23:54 +0200

    home page & footer, navbar

---
+++ src/components/getButton.tsx
@@ -0,0 +1,38 @@
+import { useBrowserInfo } from "../hooks/useBrowserInfo";
+import JsonExtension from "../assets/urlWebStoreExtensions.json";
+
+export const GetExtensionButton = () => {
+  const { isDesktop, typeBrowser, isValidBrowser } = useBrowserInfo();
+
+  const GetURL = (): string => {
+    let url = JsonExtension[typeBrowser as keyof typeof JsonExtension]
+    return  url ? url : "/browser";
+  };
+
+  const renderButton = () => {
+    const adjustedName = typeBrowser === "edge-chromium" ? "edge" : typeBrowser;
+
+    if (!adjustedName) {
+      return <span className="w-32 h-4 rounded-md bg-purple-900/30"></span>;
+    }
+
+    if (!isDesktop || !isValidBrowser)
+      return <p>View supported browsers</p>;
+
+    return (
+      <p>
+        Get for <span className="capitalize">{adjustedName}</span>
+      </p>
+    );
+  };
+
+  return (
+    <a
+      href={GetURL()}
+      target="_blank"
+      className="px-6 border border-purple-900 h-12 rounded-full hover:bg-purple-950 transition-colors flex items-center justify-center"
+    >
+      {renderButton()}
+    </a>
+  );
+};
