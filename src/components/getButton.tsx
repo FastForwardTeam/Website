045dae22f108084061fd26e1178eb07b4043a72b
Jose Garcia <47431411+ruxwez@users.noreply.github.com>
2024-05-16 23:55:10 +0200

    Update getButton.tsx

--- src/components/getButton.tsx
+++ src/components/getButton.tsx
@@ -5,8 +5,8 @@ export const GetExtensionButton = () => {
   const { isDesktop, typeBrowser, isValidBrowser } = useBrowserInfo();
 
   const GetURL = (): string => {
-    let url = JsonExtension[typeBrowser as keyof typeof JsonExtension]
-    return  url ? url : "/browser";
+    let url = JsonExtension[typeBrowser as keyof typeof JsonExtension];
+    return url ? url : "/browser";
   };
 
   const renderButton = () => {
@@ -16,8 +16,7 @@ export const GetExtensionButton = () => {
       return <span className="w-32 h-4 rounded-md bg-purple-900/30"></span>;
     }
 
-    if (!isDesktop || !isValidBrowser)
-      return <p>View supported browsers</p>;
+    if (!isDesktop || !isValidBrowser) return <p>View supported browsers</p>;
 
     return (
       <p>
@@ -30,7 +29,7 @@ export const GetExtensionButton = () => {
     <a
       href={GetURL()}
       target="_blank"
-      className="px-6 border border-purple-900 h-12 rounded-full hover:bg-purple-950 transition-colors flex items-center justify-center animate__animated animate__fadeIn"
+      className="px-6 border border-purple-900 h-12 rounded-full hover:bg-purple-950 transition-colors flex items-center justify-center "
     >
       {renderButton()}
     </a>
