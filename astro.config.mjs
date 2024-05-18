e8dae394094eb318e5cfe76390f5ca028cdb4d7b
Jose Garcia <47431411+ruxwez@users.noreply.github.com>
2024-05-18 04:44:47 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ruxwez.github.io",
+  base: "fastforward",
   integrations: [react(), tailwind()],
   server: {
     port: 80,
