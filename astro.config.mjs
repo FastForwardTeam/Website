5bc6616c4372b8451170b182c58a00bcf02a1398
Jose Garcia <47431411+ruxwez@users.noreply.github.com>
2024-05-18 02:31:39 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import million from "million/compiler"
 
 // https://astro.build/config
 export default defineConfig({
